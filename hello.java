18db210d86f3e69f36bd5b3a63106ea93d6f0732
gaohy <970658870@qq.com>
2018-01-10 16:51:58 +0800

    新增hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,17 @@
 public class hello{
-	public static void main(Object[] ars){
-		System.out.println("hello man!");
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		while(true){
+			String test = s.nextLine();
+			if("start"){
+				System.out.println(getnum());
+			}else{
+				System.out.println("没有该指令！");
+			}
+		}
+	}
+	public static int getnum(){
+		Random rm = new Random();
+		return rm.nextInt(6) + 1;
 	}
 }
