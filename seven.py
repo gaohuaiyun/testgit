6893fc635aafeadf80bbaeda6699e9372cdc7057
gaohy <970658870@qq.com>
2018-01-11 17:01:12 +0800

    功能同hello.java，生成随机数

---
+++ seven.py
@@ -0,0 +1,17 @@
+﻿# -*- coding: UTF-8 -*-
+#生成1-6之间的随机数
+
+#导入random（随机数）模块
+import random
+
+a = 1
+b = 2
+print("请输入指令：start--开始，stop--结束")
+while a < b:
+	x = input("")
+	if "start" ==x:
+		print(random.randint(1,9))
+	elif "stop" == x:
+		break
+	else:
+		print("输入错误！")
