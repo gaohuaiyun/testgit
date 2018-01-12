a2adf122cb0b7631d99589cb34a74e980e0f2dff
gaohy <970658870@qq.com>
2018-01-12 17:34:49 +0800

    导入sys

---
+++ five.py
@@ -0,0 +1,10 @@
+﻿#!/usr/bin/python3
+#coding=utf-8
+#文件名：five.py
+import sys
+#from sys import argv,path #导入特定的成员
+print('========= Python import mode ========')
+print('命令行参数为：')
+for i in sys.argv:
+	print(i)
+	print('\n python3 路径为',sys.path)
